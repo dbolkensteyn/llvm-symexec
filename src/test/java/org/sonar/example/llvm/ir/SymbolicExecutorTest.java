83de091174aae4282eb7bf411eb55dd294e7fd28
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-02-12 16:04:38 +0100

    Symbolic executor: Add more tests

--- src/test/java/org/sonar/example/llvm/ir/SymbolicExecutorTest.java
+++ src/test/java/org/sonar/example/llvm/ir/SymbolicExecutorTest.java
@@ -22,7 +22,7 @@ public class SymbolicExecutorTest {
   }
 
   @Test
-  public void should_find_basic_npe() {
+  public void should_detect_loading_from_null() {
     thrown.expectMessage("NPE: %5 = load i32* %4, align 4");
 
     FunctionDefinitionSyntax f = ParserTest.parse(
@@ -35,6 +35,38 @@ public class SymbolicExecutorTest {
       "  store i32* null, i32** %1, align 8",
       "  %4 = load i32** %1, align 8",
       "  %5 = load i32* %4, align 4",
+      "  ret void",
+      "}");
+
+    new SymbolicExecutor().evaluate(f);
+  }
+
+  @Test
+  public void should_detect_storing_to_null() {
+    thrown.expectMessage("NPE: store i32* null, i32** %2, align 8");
+
+    FunctionDefinitionSyntax f = ParserTest.parse(
+      IrGrammarRuleKeys.FUNCTION_DEFINITION,
+      "define void @f1(i32* %p) #0 {",
+      "  %1 = alloca i32*, align 8",
+      "  store i32* null, i32** %1, align 8",
+      "  %2 = load i32** %1, align 8",
+      "  store i32* null, i32** %2, align 8",
+      "  ret void",
+      "}");
+
+    new SymbolicExecutor().evaluate(f);
+  }
+
+  @Test
+  public void should_detect_stores_of_uninitialized_values() {
+    thrown.expectMessage("Cannot read an uninitialized value \"%2\"! store i32* %2, i32** %1, align 8");
+
+    FunctionDefinitionSyntax f = ParserTest.parse(
+      IrGrammarRuleKeys.FUNCTION_DEFINITION,
+      "define void @f1(i32* %p) #0 {",
+      "  %1 = alloca i32*, align 8",
+      "  store i32* %2, i32** %1, align 8",
       "  ret void",
       "}");
 
